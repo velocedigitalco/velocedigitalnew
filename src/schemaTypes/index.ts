d163c302419a9214be2a2f4be91879a4d558c054
velocedigitalco <velocedigital.co@gmail.com>
2026-07-12 14:03:09 +0000

    Delete src directory

--- src/schemaTypes/index.ts
+++
@@ -1,5 +0,0 @@
-import cta from './objects/cta'
-import capabilityCard from './objects/capabilityCard'
-import homePage from './documents/homePage'
-
-export const schemaTypes = [cta, capabilityCard, homePage]
